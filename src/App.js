23ff6cc835475e003006277cafb8cce8d8b894a5
Nur <90011276+Nur9835@users.noreply.github.com>
2023-04-21 14:13:51 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import { BookProvider } from "./context/BooksContext";
+
+import Page404 from "./page/Page404";
+import BookDetail from "./page/BookDetail";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookProvider>
+    <Router>
+
+    
+      
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<BookDetail />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+
+
+    </Router>
+  </BookProvider>
   );
 }
 
